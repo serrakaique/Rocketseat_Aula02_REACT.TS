6ccbc2efad482825809e19aa05b9b6fd7cb654fb
Kaique Luiz Nascimento Serra <3SNLKaique@ache.com.br>
2024-08-06 12:06:00 -0300

    Feito

--- 02-ignite-timer/src/components/Header/index.tsx
+++ 02-ignite-timer/src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import { Timer, Scroll } from 'phosphor-react'
 import { HeaderContainer } from './styles'
 
-import logoIgnite from '../../assets/ignite-logo.svg'
+import logoIgnite from '../../assets/logo-ignite.svg'
 import { NavLink } from 'react-router-dom'
 
 export function Header() {
